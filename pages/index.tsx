6523806add83ca799bcb3fc10fad1e60774c6f4a
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-10-10 15:52:40 +0700

    investigate typescript and styledcomponent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
 import * as React from 'react'
 import Form from '../components/Form.container'
-import withReduxApollo from '../lib/with-redux-apollo'
+import styled from 'styled-components'
+// import withReduxApollo from '../lib/with-redux-apollo'
 
-export default withReduxApollo(Form)
+// export default withReduxApollo(Form)
+
+const Input = styled.input([`
+  width: 100px;
+`])
+
+export default () => {
+
+  console.log('render')
+  return (
+    <Input />
+  )
+}
