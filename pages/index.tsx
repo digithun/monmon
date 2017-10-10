578a02ebdec1fb94edb8c0bd082913966434c84d
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-10-10 17:20:23 +0700

    refactor(comment) use slug instead of relation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react'
 import Form from '../components/Form.container'
-import styled from 'styled-components'
-// import withReduxApollo from '../lib/with-redux-apollo'
+import styledComponent from 'styled-components'
+// const styled = require('styled-components').default
+const styled = styledComponent
 
-// export default withReduxApollo(Form)
-
-const Input = styled.input([`
+const Input = styled.input`
   width: 100px;
-`])
+`
 
 export default () => {
 
